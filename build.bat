5e89dc4468624f0de540be14ed621976281fb5fb
Matti J. Kärki <mjk@iki.fi>
2017-12-04 00:59:32 +0200

    Added "clean" option to build.bat.

--- build.bat
+++ build.bat
@@ -2,6 +2,8 @@
 
 set RACO="C:\Program Files\Racket\raco.exe"
 
+if x%1==xclean goto CLEAN
+
 %RACO% exe --gui GeForceDrvChk.rkt
 %RACO% distribute GeForceDrvChk GeForceDrvChk.exe
 
@@ -12,3 +14,12 @@ copy README.md GeForceDrvChk
 
 del GeForceDrvChk.zip.CHECKSUM
 rmdir /s /q GeForceDrvChk
+
+goto END
+
+:CLEAN
+
+del /q GeForceDrvChk.zip
+del /q GeForceDrvChk.exe
+
+:END
